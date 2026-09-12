5ae45e2fcd6304af9ee33ac8917784414c539328
MbAIGC <314928345+MbAIGC@users.noreply.github.com>
2026-09-12 22:53:27 +0800

    fix(android): import android.app.Notification for foreground service call

--- android/app/src/main/java/com/songloft/iwebplayer/MainActivity.java
+++ android/app/src/main/java/com/songloft/iwebplayer/MainActivity.java
@@ -2,6 +2,7 @@ package com.songloft.iwebplayer;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
